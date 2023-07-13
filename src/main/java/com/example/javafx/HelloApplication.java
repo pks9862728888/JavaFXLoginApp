cde6158af85f3502062a5cb62d702fe1d198f4f5
pks9862728888 <pks9862728888@gmail.com>
2023-07-14 00:55:00 +0530

    - Added button

--- src/main/java/com/example/javafx/HelloApplication.java
+++ src/main/java/com/example/javafx/HelloApplication.java
@@ -1,19 +1,38 @@
 package com.example.javafx;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Stage Title");
+        StackPane stackPane = new StackPane();
+        Scene scene = new Scene(stackPane, 500, 500);
         stage.setScene(scene);
+
+        // Add button to stack pane
+        Button button = new Button();
+        stackPane.getChildren().add(button);
+
+        // Add functionality to button
+        button.setText("Click Me");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Button clicked!");
+            }
+        });
+
+        // Start application
         stage.show();
     }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,16 @@
 module com.example.javafx {
     requires javafx.controls;
     requires javafx.fxml;
-        requires javafx.web;
-            
-        requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-            requires net.synedra.validatorfx;
-            requires org.kordamp.ikonli.javafx;
-            requires org.kordamp.bootstrapfx.core;
-            requires eu.hansolo.tilesfx;
-        
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires lombok;
+//    requires eu.hansolo.tilesfx;
+
     opens com.example.javafx to javafx.fxml;
     exports com.example.javafx;
 }
